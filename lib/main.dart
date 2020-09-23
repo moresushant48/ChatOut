b7ce0bf0a62e837efaabc5ddff54d97aa4055a08
Sushant More <moresushant48@gmail.com>
2020-09-23 19:28:32 +0530

    Initial Commit
    Signed-off-by: Sushant More <moresushant48@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chatapp/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatefulWidget {
+  @override
+  _ChatAppState createState() => _ChatAppState();
+}
+
+class _ChatAppState extends State<ChatApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
