86e2bbf1a962775e2cc6ebed5c672c9ca34fe314
Sushant More <moresushant48@gmail.com>
2020-09-25 19:47:19 +0530

    Move SearchUserDelegate to SearchUser.dart
    Signed-off-by: Sushant More <moresushant48@gmail.com>

---
+++ lib/views/SearchUser.dart
@@ -0,0 +1,64 @@
+import 'package:chatapp/services/DatabaseMethods.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SearchUserDelegate extends SearchDelegate {
+  QuerySnapshot _users;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    DatabaseMethods databaseMethods = DatabaseMethods();
+
+    return StatefulBuilder(
+      builder: (context, setState) {
+        databaseMethods.getUserByUsername(query).then((searchedUsers) {
+          setState(() {
+            _users = searchedUsers;
+          });
+        });
+        return _users != null
+            ? ListView.builder(
+                itemCount: _users.docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(
+                      Icons.account_circle,
+                      size: 50.0,
+                    ),
+                    title: Text(_users.docs[index].get("name")),
+                    subtitle: Text(_users.docs[index].get("email")),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Text("");
+  }
+}
